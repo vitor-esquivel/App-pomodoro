3959c8507c683049993c0dc1d9c3ddd06ed941c1
Vitor Esquivel <vitor.esquivel10@gmail.com>
2025-11-07 16:49:57 -0300

    conclusão da tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,6 +5,8 @@ import {
     Image,
     TextInput,
     TouchableOpacity,
+    Alert,
+    ActivityIndicator,
 } from "react-native";
 
 import { styles } from "./styles";
@@ -14,6 +16,27 @@ import { themas } from "../../global/themes";
 export default function Login() {
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [loading,setLoading] = useState(false);
+
+    async function getLogin(){
+        try{
+            setLoading(true);
+
+            if(!email || !password){
+                alert("Preencha todos os campos");
+                return;
+            }
+            setTimeout(()=>{
+                Alert.alert('Logado com sucesso!')
+            },300)
+
+            
+        } catch(error){
+            console.log(error)
+        }finally{
+            setLoading(false);
+        }
+    }
 
    return(
     <View style={styles.container}> 
@@ -30,7 +53,10 @@ export default function Login() {
             <Text style={styles.titleinput}>ENDEREÇO DE E-MAIL</Text>
             <View style={styles.boxinput}>  
                 <TextInput 
-                    style={styles.input} />
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                     />
                 <MaterialIcons 
                 name="email"
                 size={20}
@@ -41,7 +67,10 @@ export default function Login() {
             <Text style={styles.titleinput}>SENHA</Text>
              <View style={styles.boxinput}>  
                 <TextInput 
-                    style={styles.input} />
+                    style={styles.input}
+                    value={password} 
+                    onChangeText={setPassword}
+                    />
                 <MaterialIcons 
                 name="remove-red-eye"
                 size={20}
@@ -51,8 +80,14 @@ export default function Login() {
             </View>
         </View>
         <View style={styles.boxbottom}> 
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.textButton}>Entrar</Text>
+            <TouchableOpacity style={styles.button} onPress={()=>getLogin()}>
+                {
+                    loading?
+                        <ActivityIndicator color={'#ffff'} size={"small"}/>
+                    :
+                        <Text style={styles.textButton}>Entrar</Text>
+                }
+                
             </TouchableOpacity>
         </View>
         <Text style={styles.textBotton}>Não tem conta? <Text style={{color:themas.colors.primary}}>Crie agora!</Text></Text>
